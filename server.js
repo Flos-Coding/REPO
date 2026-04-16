c77d43b529e6a598bcb654d43a37b2db8e9db789
Flo <141274144+Flos-Coding@users.noreply.github.com>
2026-04-16 18:34:42 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// 🔥 EINZIGE STELLE FÜR VERSION
+const APP_VERSION = "1.0.1";
+
+const GITHUB_USER = "Flos-Coding";
+const GITHUB_REPO = "REPO";
+const FILE_NAME = "workflow_addin.xlam";
+
+// Version API
+app.get("/version.json", (req, res) => {
+    res.json({
+        version: APP_VERSION,
+        download: "https://excel-license-server-ox2t.onrender.com/download/latest"
+    });
+});
+
+// Download Proxy (wichtig!)
+app.get("/download/latest", async (req, res) => {
+    try {
+        const url =
+            `https://github.com/${GITHUB_USER}/${GITHUB_REPO}` +
+            `/releases/download/v${APP_VERSION}/${FILE_NAME}`;
+
+        const response = await axios.get(url, {
+            responseType: "arraybuffer"
+        });
+
+        res.setHeader("Content-Type",
+            "application/vnd.ms-excel.addin.macroEnabled.12"
+        );
+
+        res.send(response.data);
+
+    } catch (err) {
+        res.status(500).send("Download failed");
+    }
+});
+
+app.listen(process.env.PORT || 3000);
